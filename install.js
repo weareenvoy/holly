91d80db4cf5ce7bf9ac0de16e792409ca8d56f84
Keenan <keenan@weareenvoy.com>
2017-02-06 18:15:23 -0800

    try relative postinstall dir

--- install.js
+++ install.js
@@ -1,6 +1,8 @@
 var fs = require('fs')
-console.log('dir: ' + process.cwd())
-fs.writeFile(process.cwd() + '/gulpfile.js', 'console.log("Hello, World!")', function (err) {
+
+// console.log('dir: ' + process.cwd())
+
+fs.writeFile('../../gulpfile.js', 'console.log("Hello, World!")', function (err) {
   if (err) {
     return console.log(err)
   }
