d5ef506dc4e52c79f7fc63c8a319a05ab802feb7
Keenan <keenan@weareenvoy.com>
2017-02-08 11:47:03 -0800

    add initial init task prompt flow

--- buildsys/tasks/init.js
+++ buildsys/tasks/init.js
@@ -6,8 +6,59 @@
  * 'gulp init'
  */
 
+var inquirer = require('inquirer')
+
 /* $ gulp init */
 gulp.task('init', function () {
-  return gulp.src(config.paths.hollyRoot + '/templates/src/**/*')
-    .pipe(gulp.dest(config.paths.srcRoot))
+
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'projectName',
+      message: 'Please enter formal project name (i.e. Holly Inc. Website)',
+      validate: function (answer) {
+        if (answer.length === 0) {
+          return 'Project name cannot be empty!'
+        }
+        return true
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'hasSASS',
+      message: 'Will this project require SASS compilation?'
+    },
+    {
+      type: 'confirm',
+      name: 'hasBabel',
+      message: 'Will this project require Babel (ES2015) compilation?'
+    },
+    {
+      type: 'confirm',
+      name: 'proceed',
+      message: function (answers) {
+        var confirmMsg = 'Boilerplate files to help jumpstart your development will be copied to the project root:\n'
+        if (answers.hasSASS) {
+          confirmMsg += config.styles.paths.src + '\n'
+        }
+        if (answers.hasBabel) {
+          confirmMsg += config.scripts.paths.src + '\n'
+        }
+        return confirmMsg + 'Would you like to continue?'
+      },
+      when: function (answers) {
+        return answers.hasSASS || answers.hasBabel
+      }
+    }
+  ]).then(function (answers) {
+    console.log('answers: ', answers)
+
+    if (answers.proceed) {
+      console.log('Scaffolding initial boilerplate files...')
+      return gulp.src(config.paths.hollyRoot + '/templates/src/**/*')
+        .pipe(gulp.dest(config.paths.srcRoot))
+    } else {
+      console.log('Nothing to scaffold, exiting project init flow.')
+    }
+  })
 })
