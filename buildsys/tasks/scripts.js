317256bae25f823c7e1e0c1adef6d18027dc0132
Keenan <keenan@weareenvoy.com>
2017-02-14 11:22:09 -0800

    move standardjs linting to single scripts tasks prior to running browserify

--- buildsys/tasks/scripts.js
+++ buildsys/tasks/scripts.js
@@ -6,10 +6,8 @@
  * 'gulp scripts'
  */
 
-var runSequence = require('run-sequence')
 var standard = require('gulp-standard')
 var plumber = require('gulp-plumber')
-// var babel = require('gulp-babel')
 var babelify = require('babelify')
 var uglify = require('gulp-uglify')
 var sourcemaps = require('gulp-sourcemaps')
@@ -20,30 +18,16 @@ var srcScriptsGlob = config.scripts.paths.src + '/**/*.js'
 
 /* $ gulp scripts */
 gulp.task('scripts', function () {
-  return runSequence(
-    'scripts:lint',
-    'scripts:compile'
-  )
-})
-
-/* $ gulp scripts:lint */
-gulp.task('scripts:lint', function () {
   return gulp.src(srcScriptsGlob)
+    .pipe(plumber())
     .pipe(standard())
     .pipe(standard.reporter('default', {
       breakOnError: true
     }))
-})
-
-/* $ gulp scripts:compile */
-gulp.task('scripts:compile', function () {
-  return gulp.src(srcScriptsGlob)
-    .pipe(plumber())
     .pipe(gulpif((env === 'dev'), sourcemaps.init()))
     .pipe(browserify({
       transform: [ 'babelify' ]
     }))
-    // .pipe(babel())
     .pipe(gulpif((env === 'prod'), uglify()))
     .pipe(gulpif((env === 'dev'), sourcemaps.write()))
     .pipe(gulp.dest(config.scripts.paths.output))
