e07b4b7b4bf2042ba4b4ab1743073881a04350ce
Keenan <keenan@weareenvoy.com>
2017-10-12 18:52:54 -0700

    running todo list

--- gulpfile.js
+++ gulpfile.js
@@ -6,5 +6,21 @@
   @author ENVOY web development team
  */
 
+/**
+  Running TODO List:
+  - set up initial twig layouts and partials
+  - figure out component structure for templates / js / styles
+  - add prettier?
+  - add image / svg sprite optimization
+  - add gulp notification util on error
+  - make better way to extend / override config
+  - add more / modify questions when installing for the first time
+  - add scaffolding / make commands to add new components or install existing ones
+  - optimize local dev environment
+  - test prod settings
+  - test via node_modules install
+  - improve readme
+ */
+
 global.config = require('./config');
 require('./buildsys')
