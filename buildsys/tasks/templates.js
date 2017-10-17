bc3fe4d71669e718a7522968a0356e1ec9bcd5ed
Keenan <keenan@weareenvoy.com>
2017-10-17 13:55:57 -0700

    exclude layout and partials folders when compling templates

--- buildsys/tasks/templates.js
+++ buildsys/tasks/templates.js
@@ -9,7 +9,13 @@ var browserSync = require('../browserSync').server
 
 /* $ gulp templates */
 gulp.task('templates', function () {
-  return gulp.src(config.templates.paths.src + '/**/*.twig')
+  var glob = [
+    config.templates.paths.src + '/**/*.twig',
+    '!' + config.templates.paths.src + '/layout/**/*',
+    '!' + config.templates.paths.src + '/macros/**/*',
+    '!' + config.templates.paths.src + '/partials/**/*'
+  ]
+  return gulp.src(glob)
     .pipe(twig())
     .pipe(gulp.dest(config.templates.paths.output))
     .on('end', browserSync.reload)
