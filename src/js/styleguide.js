5fda3da5b3d27837b786de249a424d570d074e08
Keenan <keenan@weareenvoy.com>
2017-10-24 14:08:54 -0700

    adding copy clipboard js for code

--- src/js/styleguide.js
+++ src/js/styleguide.js
@@ -3,7 +3,18 @@
  * Styleguide main entry point.
  */
 
+import $ from 'jquery'
 import Prism from 'prismjs'
+import Clipboard from 'clipboard'
 import { SG_Card } from '../components/Styleguide/SG_Card/SG_Card'
 
 new SG_Card('headings') // eslint-disable-line
+const clipboard = new Clipboard('.btn-copy')
+
+clipboard.on('success', function (e) {
+  const $btn = $(e.trigger)
+  $btn.text('Copied!').prop('disabled', true)
+  setTimeout(function () {
+    $btn.text('Copy').prop('disabled', false)
+  }, 2000)
+})
