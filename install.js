dc6c692abc64185d15e848ef654eabb67cce15d1
Keenan <keenan@weareenvoy.com>
2018-04-13 10:59:10 -0700

    checking if gulpfile exists before overriding and do not scaffold gitignore anymore

--- install.js
+++ install.js
@@ -1,37 +1,16 @@
-/**
-  Running TODO List:
-  - add capability to add multiple js entry points
-  - add macro to handle required fields for twig templates
-  - add prettier?
-  - css linter?
-  - add image / svg sprite optimization
-  - make better way to extend / override config
-  - create initial single styleguide page with all the base elements
-  - add scaffolding / make commands to add new components or install existing ones
-  - improve readme
- */
-
 var fs = require('fs')
 var prjPath = '../..'
 
-// Scaffold initial gulpfile
-fs.readFile('./templates/gulpfile.tpl.js', 'utf8', function (err, data) {
-  if (err) return console.error(err)
+if (!fs.existsSync(prjPath + '/gulpfile.js')) {
+  // Scaffold initial gulpfile
+  fs.readFile('./templates/gulpfile.tpl.js', 'utf8', function (err, data) {
 
-  // Write template file contents to new file
-  fs.writeFile(prjPath + '/gulpfile.js', data, 'utf8', function (err) {
     if (err) return console.error(err)
-    console.log('Created gulpfile.js')
-  })
-})
 
-// Scaffold initial .gitignore
-fs.readFile('./templates/.gitignore.tpl', 'utf8', function (err, data) {
-  if (err) return console.error(err)
-
-  // Write template file contents to new file
-  fs.writeFile(prjPath + '/.gitignore', data, 'utf8', function (err) {
-    if (err) return console.error(err)
-    console.log('Created .gitignore')
+    // Write template file contents to new file
+    fs.writeFile(prjPath + '/gulpfile.js', data, 'utf8', function (err) {
+      if (err) return console.error(err)
+      console.log('Created gulpfile.js')
+    })
   })
-})
+}
